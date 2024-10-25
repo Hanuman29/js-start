bd64801d42ca8b1005d19837fd380bca24dd2f2e
Hanuman29 <144440158+Hanuman29@users.noreply.github.com>
2024-10-25 16:35:01 +0000

    basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID = 7548
+let accountEmail = "hanuman@gmail.com"
+var account_Name = "hanuman"
+
+//accountID = 451
+
+console.log(accountEmail);
+
+/*
+var is not used because of issue in block scope and functional scope
+*/
